2e1dcf447167a9d38409a056c63c7a6d07659abd
WeslleyIvis <weslley_reive@hotmail.com>
2023-03-10 23:59:25 -0300

    image

--- script/script.js
+++ script/script.js
@@ -2,43 +2,26 @@
 const canvas = document.getElementById("screen");
 if (canvas instanceof HTMLCanvasElement) {
     const ctx = canvas.getContext('2d');
-    let size = 50;
-    const vel = 20;
-    let x = 0;
-    let y = 0;
-    let directX = 0;
-    let directY = 0;
-    function renderScreen() {
-        if (ctx instanceof CanvasRenderingContext2D && canvas instanceof HTMLCanvasElement) {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            for (let j = 0; j < 15; j += 5) {
-                ctx.beginPath();
-                ctx.fillStyle = `rgba(255, 255, 111, 0.2)`;
-                ctx.arc(x + size, y + size, size, 0, 2 * Math.PI);
-                ctx.fill();
-            }
-            x += directX;
-            y += directY;
-            if (x <= 0 && y <= 0) {
-                directX = Math.random() * vel;
-                directY = Math.random() * vel;
-            }
-            if (x <= 0) {
-                directX = Math.random() * vel;
-            }
-            if (x + size * 2 >= canvas.width) {
-                directX = -(Math.random() * vel);
-                console.log(directX);
-            }
-            if (y <= 0) {
-                directY = Math.random() * vel;
-            }
-            if (y + size * 2 >= canvas.height) {
-                directY = -(Math.random() * vel);
-            }
-            requestAnimationFrame(renderScreen);
-        }
+    const zelda = {
+        image: new Image(),
+        srcImage: './link.png',
+        cutX: 0,
+        cutY: 0,
+        widthCut: 120,
+        heightCut: 130,
+        WidthSprite: 0,
+        posX: 0,
+        posY: 0,
+        widthImage: 150,
+        heigthImage: 150,
+    };
+    function creatPersonagem({ image, srcImage, cutX, cutY, widthCut, heightCut, posX, posY, widthImage, heigthImage, ...personagem }) {
+        image.src = srcImage;
+        console.log(image.width);
+        image.addEventListener('load', () => {
+            ctx?.drawImage(image, cutX, cutY, widthCut, heightCut, posX, posY, widthImage, heigthImage);
+        });
     }
-    renderScreen();
+    creatPersonagem(zelda);
 }
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,54 +1,47 @@
 const canvas = document.getElementById("screen");
 
+interface personagem {
+    image: HTMLImageElement;
+    srcImage: string;
+    cutX: number;
+    cutY: number;
+    widthCut: number;
+    heightCut: number;
+    WidthSprite: number;
+    posX: number;
+    posY: number;
+    widthImage: number;
+    heigthImage: number;
+}
+
 
 if (canvas instanceof HTMLCanvasElement) {
     const ctx = canvas.getContext('2d');
-    let size = 50;
-    const vel = 20;
-    let x = 0;
-    let y = 0;
-    let directX = 0;
-    let directY = 0;
+    
 
-    function renderScreen() {
-        if(ctx instanceof CanvasRenderingContext2D && canvas instanceof HTMLCanvasElement) {
-            ctx.clearRect(0, 0, canvas.width, canvas.height)
-            
-            for(let j = 0; j < 15; j+= 5) {
-                ctx.beginPath();
-                ctx.fillStyle = `rgba(255, 255, 111, 0.2)`
-                ctx.arc(x + size, y + size,  size, 0, 2*Math.PI);
-                ctx.fill();    
-                
-            }
-              
-            x+= directX; y += directY;
-
-            if (x <= 0 && y <= 0) {
-                directX = Math.random() * vel;
-                directY = Math.random() * vel;
-            }
-
-            if (x <= 0) {
-                directX = Math.random() * vel;
-            }
-
-            if (x + size * 2 >= canvas.width) {
-                directX = -(Math.random() * vel);
-                console.log(directX)
-            }
-
-            if (y <= 0) {
-                directY = Math.random() * vel;
-            }
-
-            if (y + size * 2 >= canvas.height) {
-                directY = -(Math.random() * vel);
-            }
-            requestAnimationFrame(renderScreen)
-        }
+    const zelda: personagem = {
+        image: new Image(),
+        srcImage: './link.png',
+        cutX: 0,
+        cutY: 0,
+        widthCut: 120,
+        heightCut: 130,
+        WidthSprite: 0,
+        posX: 0,
+        posY: 0,
+        widthImage: 150,
+        heigthImage: 150, 
     }
 
-    renderScreen();
+    function creatPersonagem({image, srcImage, cutX, cutY, widthCut, heightCut, posX, posY, widthImage, heigthImage, ...personagem}: personagem) {
+        image.src = srcImage;
+        console.log(image.width )
+        image.addEventListener('load', () => {
+            // Image | XiniRecort | YiniRecort | WidthCut | HeightCut |  posX | posY | WidthImage | HeightImage 
+            ctx?.drawImage(image, cutX, cutY, widthCut, heightCut, posX, posY, widthImage, heigthImage);
+        })
+        
+    }
 
+    creatPersonagem(zelda); 
 }
